4e316db6b5167f3971498d07d6e94a81854726f5
Andrew Drobozin <crash1601@gmail.com>
2018-04-06 09:50:21 +0300

    04_06

--- src/main/java/ru/sbt/jschool/session5/problem1/SQLGenerator.java
+++ src/main/java/ru/sbt/jschool/session5/problem1/SQLGenerator.java
@@ -1,21 +1,107 @@
 package ru.sbt.jschool.session5.problem1;
 
+
+import java.lang.reflect.Field;
+
 /**
  */
 public class SQLGenerator {
     public <T> String insert(Class<T> clazz) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO ").append(clazz.getAnnotation(Table.class).name());
+        Field[] fields = clazz.getDeclaredFields();
+        int count = 0;
+        for(Field f : fields){
+            if(f.isAnnotationPresent(Column.class)) {
+                if(f.getAnnotation(Column.class).name().length() > 0){
+                    sb.append(count++ == 0 ? "(" : ", ").append(f.getAnnotation(Column.class).name().toLowerCase());
+                }
+                else{
+                    sb.append(count++ == 0 ? "(" : ", ").append(f.getName().toLowerCase());
+                }
+            }
+            else if(f.isAnnotationPresent(PrimaryKey.class)) {
+                if(f.getAnnotation(PrimaryKey.class).name().length() > 0){
+                    sb.append(count++ == 0 ? "(" : ", ").append(f.getAnnotation(PrimaryKey.class).name().toLowerCase());
+                }
+                else{
+                    sb.append(count++ == 0 ? "(" : ", ").append(f.getName().toLowerCase());
+                }
+            }
+        }
+        sb.append(") VALUES (");
+        while (count != 0){
+            if (count == 1){
+                sb.append("?)");
+                break;
+            }
+            sb.append("?, ");
+            count--;
+        }
+        return sb.toString();
     }
 
     public <T> String update(Class<T> clazz) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("UPDATE ").append(clazz.getAnnotation(Table.class).name()).append(" SET");
+        Field[] fields = clazz.getDeclaredFields();
+        int count = 0;
+        for(Field f : fields){
+            if(f.isAnnotationPresent(Column.class)){
+                if(f.getAnnotation(Column.class).name().length() > 0){
+                    sb.append(count++ == 0 ? " " : ", ").append(f.getAnnotation(Column.class).name().toLowerCase()).append(" = ?");
+                }else {
+                    sb.append(count++ == 0 ? " " : ", ").append(f.getName().toLowerCase()).append(" = ?");
+                }
+            }
+        }
+        sb.append(" WHERE");
+        count = 0;
+        for(Field f : fields){
+            if(f.isAnnotationPresent(PrimaryKey.class)){
+                sb.append(count++==0?" ":" AND ").append(f.getName().toLowerCase()).append(" = ?");
+            }
+        }
+        return sb.toString();
     }
 
     public <T> String delete(Class<T> clazz) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("DELETE");
+        Field[] fields = clazz.getDeclaredFields();
+        sb.append(" FROM ").append(clazz.getAnnotation(Table.class).name());
+        sb.append(" WHERE");
+        int count = 0;
+        for(Field f : fields){
+            if(f.isAnnotationPresent(PrimaryKey.class)){
+                sb.append(count++==0?" ":" AND ").append(f.getName().toLowerCase()).append(" = ?");
+            }
+        }
+        return sb.toString();
     }
 
     public <T> String select(Class<T> clazz) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT");
+        Field[] fields = clazz.getDeclaredFields();
+        int count = 0;
+        for(Field f : fields){
+            if(f.isAnnotationPresent(Column.class)){
+                if(f.getAnnotation(Column.class).name().length() > 0){
+                    sb.append(count++ == 0 ? " " : ", ").append(f.getAnnotation(Column.class).name().toLowerCase());
+                }else {
+                    sb.append(count++ == 0 ? " " : ", ").append(f.getName().toLowerCase());
+                }
+            }
+        }
+        sb.append(" FROM ").append(clazz.getAnnotation(Table.class).name());
+        sb.append(" WHERE");
+        count = 0;
+        for(Field f : fields){
+            if(f.isAnnotationPresent(PrimaryKey.class)){
+                sb.append(count++==0?" ":" AND ").append(f.getName().toLowerCase()).append(" = ?");
+            }
+        }
+        return sb.toString();
     }
 }
